9f410e111eb68debd266746384cd0290b544af83
Наиль <nailuspnov@gmail.com>
2022-07-22 18:18:05 +0400

    TODO-109: postgres setup

--- cmd/main.go
+++ cmd/main.go
@@ -3,26 +3,48 @@ package main
 import (
 	"aero-internship/gen/api"
 	"aero-internship/internal/adapters/handlers/v1"
+	"aero-internship/pkg/client/postgres"
 	"context"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
-	"log"
 	"net"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	// инициализация env конфига
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	//соединение с бд
+	_, err := postgres.NewPostgresDB(postgres.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Username: os.Getenv("DB_USERNAME"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   os.Getenv("DB_NAME"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+	})
+
+	if err != nil {
+		logrus.Fatalf("failed to initialize db.sql %s", err.Error())
+	}
+
 	srv := &v1.GRPCServer{}
 	go func() {
 		mux := runtime.NewServeMux()
 		api.RegisterContentCheckServiceHandlerServer(context.Background(), mux, srv)
-
-		log.Fatalln(http.ListenAndServe("localhost:8000", mux))
+		logrus.Fatalln(http.ListenAndServe("localhost:8000", mux))
 	}()
 
 	l, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		log.Fatal(err)
+		logrus.Fatal(err)
 	}
 
 	s := grpc.NewServer()
@@ -30,6 +52,6 @@ func main() {
 	api.RegisterContentCheckServiceServer(s, srv)
 
 	if err := s.Serve(l); err != nil {
-		log.Fatal(err)
+		logrus.Fatal(err)
 	}
 }
